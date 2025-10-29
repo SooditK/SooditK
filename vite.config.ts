0da90a501257692c8886066663a797048b6f22a6
Soodit Kumar <sooditkumarabc@gmail.com>
2025-10-29 11:07:03 +0530

    💕
    Signed-off-by: Soodit Kumar <sooditkumarabc@gmail.com>

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,12 @@ import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import viteReact from '@vitejs/plugin-react'
 import viteTsConfigPaths from 'vite-tsconfig-paths'
 import tailwindcss from '@tailwindcss/vite'
+import { devtools } from '@tanstack/devtools-vite'
 
 const config = defineConfig(async () => {
   return {
     plugins: [
+      devtools(),
       cloudflare({ viteEnvironment: { name: "ssr" } }),
       // this is the plugin that enables path aliases
       viteTsConfigPaths({
